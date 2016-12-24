6ecc487c0c903e65b02780201a17c1bce6dc0fd8
paleo13 <jeffrey.hanson@uqconnect.edu.au>
2016-12-24 14:25:11 +1000

    code change

--- app/www/js-interface.js
+++ app/www/js-interface.js
@@ -7,7 +7,7 @@ Shiny.addCustomMessageHandler("jsCode",
 
 Shiny.addCustomMessageHandler("update_var",
 	function(message) {
-		eval(message.var + "=" + message.val);
+		eval(message.var + '=' + message.val);
 	}
 	);
 
@@ -16,7 +16,7 @@ var is_dirty=false;
 var auto_send=false;
 function exit_page(event) {
 	if (is_dirty && !auto_send) {
-		return \'You have made changes to the data without downloading or emailing it -- if you leave before performing either of these actions all data will be lost.\'
+		return 'You have made changes to the data without downloading or emailing it -- if you leave before performing either of these actions all data will be lost.'
 	}
 }
 window.onbeforeunload = exit_page;
@@ -38,18 +38,18 @@ Shiny.addCustomMessageHandler("enable_button",
 
 Shiny.addCustomMessageHandler("disable_button", 
 	function(message) {
-		$("#" + message.btn).prop(\"disabled\",true);
+		$("#" + message.btn).prop('disabled',true);
 	}
 );
 			
 var enterTextInputBinding = new Shiny.InputBinding();
 	$.extend(enterTextInputBinding, {
 	find: function(scope) {
-		return $(scope).find(\'.enterTextInput\');
+		return $(scope).find('.enterTextInput');
 	},
 	getId: function(el) {
 		//return InputBinding.prototype.getId.call(this, el) || el.name;
-		return $(el).attr(\'id\')
+		return $(el).attr('id')
 	},
 	getValue: function(el) {
 		return el.value;
@@ -58,34 +58,34 @@ var enterTextInputBinding = new Shiny.InputBinding();
 		el.value = value;
 	},
 	subscribe: function(el, callback) {
-		$(el).on(\'keyup.textInputBinding input.textInputBinding\', function(event) {
+		$(el).on('keyup.textInputBinding input.textInputBinding', function(event) {
 			if(event.keyCode == 13) { //if enter
 				callback()
 			}
 		});	
 	},
 	unsubscribe: function(el) {
-		$(el).off(\'.enterTextInputBinding\');
+		$(el).off('.enterTextInputBinding');
 	},
 	receiveMessage: function(el, data) {
-		if (data.hasOwnProperty(\'value\'))
+		if (data.hasOwnProperty('value'))
 			this.setValue(el, data.value);
-		if (data.hasOwnProperty(\'label\'))
-			$(el).parent().find(\'label[for=\' + el.id + \']\').text(data.label);
-		$(el).trigger(\'change\');
+		if (data.hasOwnProperty('label'))
+			$(el).parent().find('label[for=' + el.id + ']').text(data.label);
+		$(el).trigger('change');
 	},
 	getState: function(el) {
 		return {
-			label: $(el).parent().find(\'label[for=\' + el.id + \']\').text(),
+			label: $(el).parent().find('label[for=' + el.id + ']').text(),
 			value: el.value
 		};
 	},
 	getRatePolicy: function() {
 		return {
-			policy: \'debounce\',
+			policy: 'debounce',
 			delay: 250
 		};
 	}
 });
 
-Shiny.inputBindings.register(enterTextInputBinding, \'shiny.enterTextInput\');
+Shiny.inputBindings.register(enterTextInputBinding, 'shiny.enterTextInput');
