f34ebc6d4e10dde2b2a7bb6e29d3de753f901ec6
paleo13 <jeffrey.hanson@uqconnect.edu.au>
2016-12-24 14:05:43 +1000

    code change

--- app/ui.R
+++ app/ui.R
@@ -3,6 +3,7 @@ library(leaflet)
 shinyUI(basicPage(
 	# load styles and js scripts
 	tags$head(
+		addResourcePath('www'),
 		includeScript('www/google-analytics.js'),
 		includeScript('www/js-interface.js'),
 		includeCSS('www/main-styles.css'),
